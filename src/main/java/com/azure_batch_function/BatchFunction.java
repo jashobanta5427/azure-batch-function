db51cae2bf2feff2154a03127afb95b0aa335f05
Jashobanta Behera <jashobanta5427@gmail.com>
2025-04-10 15:04:23 +0530

    FINAL WORKING CODE

--- src/main/java/com/azure_batch_function/BatchFunction.java
+++ src/main/java/com/azure_batch_function/BatchFunction.java
@@ -1,18 +1,32 @@
 package com.azure_batch_function;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
 
+import com.microsoft.azure.functions.ExecutionContext;
+
+import java.util.Locale;
 import java.util.function.Consumer;
 
 @Configuration
 public class BatchFunction {
 
+    private static final Logger logger = LoggerFactory.getLogger(BatchFunction.class);
+
     @Bean
-    Consumer<String> processBatch() {
-        return input -> {
-            System.out.println("✅ Batch job executed at: " + java.time.LocalDateTime.now());
-            // Your batch processing logic here
+    public Consumer<Message<String>> processBatch() {
+        return message -> {
+        	String timeInfo = message.getPayload();
+			String value = timeInfo.toUpperCase(Locale.ROOT);
+
+			logger.info("Timer is triggered with TimeInfo: " + value);
+
+            ExecutionContext context = (ExecutionContext) message.getHeaders().get(BatchTimerHandler.EXECUTION_CONTEXT);
+			context.getLogger().info("✅ Batch job executed at: " + java.time.LocalDateTime.now());
+            
         };
     }
 }
